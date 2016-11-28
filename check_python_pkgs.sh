49491923714830cdee736ee9bb96afa7569774ee
Ramanathan Muthiah <rus.cahimb@gmail.com>
2016-11-28 18:32:40 +0530

    resolve merge conflict

--- check_python_pkgs.sh
+++ check_python_pkgs.sh
@@ -3,11 +3,7 @@
 ## apt list --installed : Returns warnings about aptitude
 ## apt-mark showmanual : displays the name of packages only
 ## dpkg -l : Returns oneline note on each installed pkg
-<<<<<<< HEAD
 python_pkgs=`dpkg --get-selections | grep -i python`
-=======
-python_pkgs=`dpkg --get-selections | grep -i python
->>>>>>> 567bfae4bea42191e72fdb591078206ebe79d2a9
 
 echo "Python packages installed..."
 echo ${python_pkgs}
