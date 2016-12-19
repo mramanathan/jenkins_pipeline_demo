b91214a3ebda00741aeb8beb1a9b401d6152aaf8
Ramanathan Muthiah <rus.cahimb@gmail.com>
2016-12-19 14:51:05 +0530

    trial 17: missing class ?

--- vars/buildUtils.groovy
+++ vars/buildUtils.groovy
@@ -1,13 +1,15 @@
 #!groovy
 
-def call() {
+class buildUtils {
+    def call() {
 
-    sh 'git rev-parse HEAD > commit_id'
+        sh 'git rev-parse HEAD > commit_id'
 
-    try {
-        env.COMMIT_ID = readFile('commit_id').trim()
-	echo "${COMMIT_ID}"
-    } catch (FileNotFoundException e) {
-        return
-    }
+            try {
+                env.COMMIT_ID = readFile('commit_id').trim()
+	        echo "${COMMIT_ID}"
+            } catch (FileNotFoundException e) {
+                return
+            }
+   }
 }
