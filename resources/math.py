77d1224f028b324a0595a558adebf66348542eda
Ramanathan Muthiah <rus.cahimb@gmail.com>
2016-12-22 15:06:06 +0530

    Trials and Triabulations 3: shared python library

---
+++ resources/math.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+high_val = 3729
+low_val  = 0109
+mul_val  = 65
+
+print("Maximum value when the plane was low on fuel, {}".format(high_val))
+print("Minimum value when the plane was high on fuel, {}".format(low_val))
