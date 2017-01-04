9373e85812752056012a878aad214eaf123bf8c0
Ramanathan Muthiah <rus.cahimb@gmail.com>
2017-01-04 07:39:50 +0530

    move to api 18 and new project switch

--- rundeck_job.sh
+++ rundeck_job.sh
@@ -6,8 +6,8 @@
 ## ========
 # Howto overcome this via pipeline script ? using withEnv ??
 ## ========
-export RD_URL=http://54.202.181.216:4440/api/17
+export RD_URL=http://54.202.181.216:4440/api/18
 #export RD_USER=admin
 #export RD_PASSWORD=admin
 export RD_TOKEN=SBrHrDFwDHr9PMcBI1V3AsKhk6YoIf3J
-rd run -p ansible_play -i 5c970e5b-8d36-4a28-8b5f-905a9c81949e  -- -ARG1 patch_manifest
+rd run -p pipeline_poc -i 5c970e5b-8d36-4a28-8b5f-905a9c81949e  -- -ARG1 patch_manifest
