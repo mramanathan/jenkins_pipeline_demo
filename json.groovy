9399742b45d5864376d944dca983f2a616dbc38f
Ramanathan.M <ramanathan.muthiah@fireeye.com>
2017-09-11 21:13:42 +0530

    groovy's idiosyncracies

--- json.groovy
+++ json.groovy
@@ -4,7 +4,7 @@ def jsonLint(String jsonfile) {
 
   echo "JSON file to lint, ${jsonfile}"
 
-  def lint_status = sh(returnStatus: true, script: 'jsonlint-py ${jsonfile}')
+  def lint_status = sh(returnStatus: true, script: "jsonlint-py ${jsonfile}")
 
   echo "JSON lint status, ${lint_status}"
 
