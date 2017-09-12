20ff15e9d868963e2cd8883b6304fd74079142fb
Ramanathan.M <ramanathan.muthiah@fireeye.com>
2017-09-12 14:00:34 +0530

    try return from ext groovy method

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,8 +7,12 @@ node {
           git url: 'https://github.com/mramanathan/jenkins_pipeline_demo', branch: 'callgroovy'
           def jsonfile = "test.json"
           def json = load "json.groovy"
+          lintok = json.jsonLint("${jsonfile}")
+          echo "Status of json lint, ${lintok}"
+          /*
           json.jsonLint("${jsonfile}")
           echo "Status of json lint, ${env.lint_status}"
+          */
         }
       }
     }
--- json.groovy
+++ json.groovy
@@ -4,9 +4,16 @@ def jsonLint(String jsonfile) {
 
   echo "JSON file to lint, ${jsonfile}"
 
-  env.lint_status = sh(returnStatus: true, script: "jsonlint-py ${jsonfile}")
+  def lint_status = sh(returnStatus: true, script: "jsonlint-py ${jsonfile}")
 
+  echo "JSON lint status, ${lint_status}"
+
+  return "${lint_status}"
+
+  /*
+  env.lint_status = sh(returnStatus: true, script: "jsonlint-py ${jsonfile}")
   echo "JSON lint status, ${env.lint_status}"
+  */
 
 }
 
