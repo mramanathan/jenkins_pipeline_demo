c37f26a7bac9d1912a508e4390a7467a6395f7f2
Ramanathan.M <ramanathan.muthiah@fireeye.com>
2017-09-13 14:11:47 +0530

    add stages in ext groovy

--- json.groovy
+++ json.groovy
@@ -15,30 +15,64 @@ def lintYml(String ymlfile) {
 }
 
 
+def cfgDeploy(String ymlfile) {
+
+    echo "~> YML file to deploy, ${ymlfile}"
+    def cfgdeploy_status = "OKAY"
+    echo "~> YML lint status, ${cfgdeploy_status}"
+
+    return cfgdeploy_status
+
+}
+
+
 def lint_deploy(String ymlfile) {
     timeout(time: 120, unit: 'SECONDS') {
-        // waitUntil {
             def ymllint_status = ""
+            def cfgdeploy_status = ""
 
-            try {
-                ymllint_status = lintYml("${ymlfile}")
-                echo "YAML lint status, ${ymllint_status}"
-            } catch (Exception err) {
-                ymllint_status = "NOT DONE"
-                echo "~> YML lint status, ${ymllint_status}"
-                throw err
+            stage('Lint') {
+                try {
+                    ymllint_status = lintYml("${ymlfile}")
+                    echo "YAML lint status, ${ymllint_status}"
+                } catch (Exception err) {
+                    ymllint_status = "NOT DONE"
+                    echo "~> YAML lint status, ${ymllint_status}"
+                    throw err
+                }
+
+                if ( "${ymllint_status}" == "2" || "${ymllint_status}" == "0" ) {
+                    echo "~> ${ymlfile} linting PASSED"
+                    currentBuild.result = "SUCCESS"
+                    return
+                } else {
+                    echo "~> ${ymlfile} linting FAILED"
+                    currentBuild.result = "FAILURE"
+                    return
+                }
             }
 
-            if ( "${ymllint_status}" == "2" || "${ymllint_status}" == "0" ) {
-                echo "~> ${ymlfile} linting PASSED"
-                currentBuild.result = "SUCCESS"
-                return
-            } else {
-                echo "~> ${ymlfile} linting FAILED"
-                currentBuild.result = "FAILURE"
-                return
+            stage('Deploy') {
+                try {
+                    cfgdeploy_status = cfgDeploy("${ymlfile}")
+                    echo "cfg deploy status, ${cfgdeploy_status}"
+                } catch (Exception err) {
+                    cfgdeploy_status = "NOT DONE"
+                    echo "~> cfg deploy status, ${cfgdeploy_status}"
+                    throw err
+                }
+
+                if ( "${cfgdeploy_status}" == "OKAY" ) {
+                    echo "~> ${ymlfile} deploy PASSED"
+                    currentBuild.result = "SUCCESS"
+                    return
+                }
+                if ( "${cfgdeploy_status}" == "YAKO" ) {
+                    echo "~> ${ymlfile} deploy FAILED"
+                    currentBuild.result = "FAILURE"
+                    return
+                }
             }
-        // }
     }
 }
 
