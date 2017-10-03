e04091249a5bace7608ec6b97e244aaeafd95f12
Ramanathan.M <ramanathan.muthiah@fireeye.com>
2017-10-03 10:13:39 +0530

    check for repo solution in env

--- runme.sh
+++ runme.sh
@@ -2,3 +2,10 @@
 
 echo "Dumping environment vars and values..."
 printenv
+
+if [[ "$vmharbor" = "YES" ]]
+then
+   echo "make changes to /etc/default/docker"
+else
+   echo "make no changes to /etc/default/docker"
+fi
